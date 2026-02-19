08fe414ced8cb62b0775c498a453a660aa94bb0d
SUBHAVA OJHA <subhava.22gcebcs190@galgotiacollege.edu>
2026-02-19 18:21:43 +0530

    added the chat screen

---
+++ lib/screens/chat.dart
@@ -0,0 +1,18 @@
+// for displaying chat messages
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('FlutterChat'),
+        ),
+      body: const Center(
+        child: Text('Logged in!'),
+      ),
+    );
+  }
+}
