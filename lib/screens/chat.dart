ba8794a07634d1d95ef0299182190525f0fc7016
SUBHAVA OJHA <subhava.22gcebcs190@galgotiacollege.edu>
2026-02-19 18:52:01 +0530

    Added Log out button and logic

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,4 +1,5 @@
 // for displaying chat messages
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
@@ -9,6 +10,17 @@ class ChatScreen extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(
           title: Text('FlutterChat'),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  FirebaseAuth.instance.signOut();
+                } ,
+                icon: Icon(
+                    Icons.exit_to_app,
+                    color: Theme.of(context).colorScheme.primary,
+                ),
+            ),
+          ],
         ),
       body: const Center(
         child: Text('Logged in!'),
